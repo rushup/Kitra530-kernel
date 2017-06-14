48d62f90761c5fab70802398e1cc4c4c42ac83d1
Matteo Fumagalli <matteofumagalli1275@gmail.com>
2017-06-14 13:56:11 +0200

    new sens

---
+++ send_bin.sh
@@ -0,0 +1,3 @@
+scp arch/arm/boot/zImage root@192.168.0.177:/root/
+scp usr/modules.img root@192.168.0.177:/root/
+scp arch/arm/boot/dts/s5p4418-artik530*.dtb root@192.168.0.177:/root/
---
+++ send_dtb.sh
@@ -0,0 +1,1 @@
+scp arch/arm/boot/dts/s5p4418-artik530*.dtb root@192.168.1.21:/root/
